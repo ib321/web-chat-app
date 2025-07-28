04084be4e6221db6f512452dbc2e44720ca18d26
Indian Bittu <newdb29@gmail.com>
2025-07-28 19:11:20 +0530

    Revert "V2.3 : Added initial code for keycloak."
    This reverts commit dec64735ad36fc71dfee1618879c953d26eada31.

--- AngularUI/webchat/src/app/app-routing.module.ts
+++ AngularUI/webchat/src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatboxComponent } from './chatbox/chatbox.component';
-import { KeycloakAuthGuard } from 'keycloak-angular/lib/core/services/keycloak-auth-guard';
 
 const routes: Routes = [
-  {path: "chat",component: ChatboxComponent, canActivate: [KeycloakAuthGuard]}
+  {path: "chat",component: ChatboxComponent}
 ];
 
 @NgModule({
--- AngularUI/webchat/src/app/app.module.ts
+++ AngularUI/webchat/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,23 +6,7 @@ import { AppComponent } from './app.component';
 import { ChatboxComponent } from './chatbox/chatbox.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 
-function initializeKeycloak(keycloak: KeycloakService) {
-  return () =>
-    keycloak.init({
-      config: {
-        url: 'http://localhost:8086/auth',
-        realm: 'Pathwave',
-        clientId: 'webchat',
-      },
-      initOptions: {
-        onLoad: 'login-required',  // allowed values 'login-required', 'check-sso';
-        flow: "standard",          // allowed values 'standard', 'implicit', 'hybrid';
-        checkLoginIframe: false
-      },
-    });
-}
 
 @NgModule({
   declarations: [
@@ -33,17 +17,9 @@ function initializeKeycloak(keycloak: KeycloakService) {
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule,
-    KeycloakAngularModule
+    HttpClientModule
   ],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeKeycloak,
-      multi: true,
-      deps: [KeycloakService]
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
